83e959c9472f0e8172cf95d639c4caa7d0487233
aman-kumar-bbi <aman.kumar@media4u.com>
2022-08-03 10:40:51 +0530

    Orientation done

--- lib/widget/searchBar.dart
+++ lib/widget/searchBar.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:database_json/constant/staticValue.dart';
@@ -9,157 +8,139 @@ import 'package:provider/provider.dart';
 
 import '../providers/SearchListProvider.dart';
 
-
 class SearchBar extends StatefulWidget {
   final searchFeildController;
   final List<dynamic> wholeListFromJson;
   String deviceType;
-  SearchBar(
-      {Key? key,
-      required this.searchFeildController,
-      required this.wholeListFromJson,
-      required this.deviceType,
-      })
-      : super(key: key);
+  SearchBar({
+    Key? key,
+    required this.searchFeildController,
+    required this.wholeListFromJson,
+    required this.deviceType,
+  }) : super(key: key);
 
   @override
   State<SearchBar> createState() => _SearchBarState();
 }
 
-late StreamController myStreamController=StreamController();
-late Stream myStream = myStreamController.stream.asBroadcastStream(); 
-  double customWidth=200;
-    double customWithForSearchBox=200;
+late StreamController myStreamController = StreamController();
+late Stream myStream = myStreamController.stream.asBroadcastStream();
+double customWidth = 200;
+double customWithForSearchBox = 200;
+bool? isportrait;
+
 class _SearchBarState extends State<SearchBar> {
   @override
-  Map<String, dynamic> ListForSearch = {};
   Widget build(BuildContext context) {
-  List idList=widget.wholeListFromJson;
-
-  
-    return OrientationBuilder(
-      builder: (context, orientation) {
-        print("orientation $orientation");
-        if (orientation==Orientation.portrait) {
-          return ChangeNotifierProvider(
-        create: (context) => searchList(idList: idList,wholeListFromJson: widget.wholeListFromJson),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 8.0, bottom: 8),
-          child:wholeContainerConatinWhiteBox(idList,customWidth,customWithForSearchBox,true)
-        ),
-      );
-        }else {
-          return ChangeNotifierProvider(
-        create: (context) => searchList(idList: idList,wholeListFromJson: widget.wholeListFromJson),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 8.0, bottom: 8),
-          child:wholeContainerConatinWhiteBox(idList,customWidth,customWithForSearchBox,false)
-        ),
-      );
-        }
-      },
-
-    );
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      print("portrait h");
+      isportrait = true;
+    } else {
+      print("landscape h");
+      isportrait = false;
+    }
+    List idList = widget.wholeListFromJson;
+    double width = MediaQuery.of(context).size.width;
+    return 
+           ChangeNotifierProvider(
+            create: (context) => searchList(
+                idList: idList, wholeListFromJson: widget.wholeListFromJson),
+            child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, bottom: 8),
+                child: wholeContainerConatinWhiteBox(
+                    idList, customWidth, customWithForSearchBox, isportrait!,context)),
+          );     
   }
-  broadcastList(){
-     return myStreamController.sink.add(staticValue.searchedList);
+
+  broadcastList() {
+    return myStreamController.sink.add(staticValue.searchedList);
   }
-  cancelbroadcastList(){
+
+  cancelbroadcastList() {
     return myStreamController.sink.add(widget.wholeListFromJson);
   }
-  // checkPhoneOrtablet(){
-  //   if (widget.deviceType=="phone") {
-  //   customWidth=300;
-  //   customWithForSearchBox=200;
-  // }else{
-  //   customWidth=700;
-  //   customWithForSearchBox=600;
-  // }
-  // }
 
+  Widget wholeContainerConatinWhiteBox(List idList, double customWidth,
+      double customWithForSearchBox, bool isLandscape,BuildContext context) {
+          double width = MediaQuery.of(context).size.width;
 
-Widget wholeContainerConatinWhiteBox(List idList,double customWidth,double customWithForSearchBox,bool isLandscape){
-  print("islandscape bool $isLandscape");
-  if (isLandscape==false) {
-    customWidth=1200;
-  }else{
-     if (widget.deviceType=="phone") {
-    customWidth=300;
-    customWithForSearchBox=200;
-  }else{
-    customWidth=700;
-    customWithForSearchBox=600;
-  }
-  }
-  return Container(
-            width: customWidth,
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-            child: Row(
-              children: [
-                const SizedBox(
-                  width: 50,
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                ),
-                // whiteSearchBox(idList,customWithForSearchBox,isLandscape),
-                Container(
-                  width: 50,
-                  child: IconButton(
-                      onPressed: () {
-                        
-                        setState(() {
-                          Stream<dynamic>?  myStream=cancelbroadcastList();
-                        });
-                        widget.searchFeildController.clear();
-                      },
-                      icon: const Icon(
-                        Icons.clear,
-                        color: Colors.grey,
-                      )),
-                ) 
-              ],
-            ));
-}
-
-  Widget whiteSearchBox(List idList,double customWithForSearchBox,isLandscape){
-    if (isLandscape==false) {
-    customWidth=1200;
-  }else{
-     if (widget.deviceType=="phone") {
-    customWidth=300;
-    customWithForSearchBox=200;
-  }else{
-    customWidth=700;
-    customWithForSearchBox=600;
-  }
-  }
+    if (isLandscape == false) {
+      customWidth = 1200;
+      customWithForSearchBox = 1100;
+    } else {
+      if (widget.deviceType == "phone") {
+        customWidth = width*0.8;
+        customWithForSearchBox = width*0.78;
+      } else {
+        customWidth = width*0.9;
+        customWithForSearchBox = width*0.65;
+      }
+    }
     return Container(
-                  width: customWithForSearchBox,
-                  child: TextFormField(
-                     cursorColor: Colors.black,
-    
-                      onChanged: (value) {
-
-                        setState(() {
-                          
-                        staticValue.searchedList = Provider.of<searchList>(context, listen: false).searchFunction(value,idList,widget.wholeListFromJson);
-                        });
-                         Stream<dynamic>? myStream=broadcastList() ;
-                      },
-                      
-                      controller: widget.searchFeildController,
-                      decoration: const InputDecoration(
-                        
-                        border: InputBorder.none,
-                      )),
-                );
+        width: customWidth,
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Row(
+          children: [
+            const SizedBox(
+              width: 50,
+              child: Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ),
+            whiteSearchBox(idList,customWithForSearchBox,isLandscape,context),
+            Container(
+              width: 50,
+              child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      Stream<dynamic>? myStream = cancelbroadcastList();
+                    });
+                    widget.searchFeildController.clear();
+                  },
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.grey,
+                  )),
+            )
+          ],
+        ));
   }
+
+  Widget whiteSearchBox(
+      List idList, double customWithForSearchBox, isLandscape,BuildContext context) {
+          double width = MediaQuery.of(context).size.width;
+
+    if (isLandscape == false) {
+      customWidth =  width*1;
+      customWithForSearchBox = width*0.85;
+    } else {
+      if (widget.deviceType == "phone") {
+          customWidth = width*0.8;
+        customWithForSearchBox = width*0.54;
+      } else {
+        customWidth = width*0.9;
+        customWithForSearchBox = width*0.77;
+      }
+    }
+    return Container(
+      width: customWithForSearchBox,
+      child: TextFormField(
+          cursorColor: Colors.black,
+          onChanged: (value) {
+            setState(() {
+              staticValue.searchedList =
+                  Provider.of<searchList>(context, listen: false)
+                      .searchFunction(value, idList, widget.wholeListFromJson);
+            });
+            Stream<dynamic>? myStream = broadcastList();
+          },
+          controller: widget.searchFeildController,
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+          )),
+    );
   }
-
-
-
-
+}
